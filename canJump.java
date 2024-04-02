c2bd38159326fb17021eacac43ab461e1b5541f3
AndyYin98 <128557692+AndyYin98@users.noreply.github.com>
2024-04-02 11:11:24 -0700

    Add files via upload

---
+++ canJump.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max =0;
+        for(int i =0; i<nums.length; i++){
+            if(i > max) return false;
+            int curr = i + nums[i];
+            max= Math.max(max,curr);
+        }
+        return max >= nums.length -1;
+    }
+}
